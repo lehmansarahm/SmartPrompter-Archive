1354bd69b16188fc10b3ceae80adccd458b62fb7
Sarah Lehman <lehmansarahm@gmail.com>
2019-08-25 18:02:22 -0400

    Tweaks

--- SmartPrompter_V2/sp_res_lib/src/main/java/edu/temple/sp_res_lib/utils/StorageUtil.java
+++ SmartPrompter_V2/sp_res_lib/src/main/java/edu/temple/sp_res_lib/utils/StorageUtil.java
@@ -13,44 +13,34 @@ import java.util.ArrayList;
 import edu.temple.sp_res_lib.obj.Alarm;
 import edu.temple.sp_res_lib.obj.SurveyQuestion;
 
-import static edu.temple.sp_res_lib.utils.Constants.LOG_TAG;
-import static edu.temple.sp_res_lib.utils.Constants.SP_KEY_GUIDS;
-
 public class StorageUtil {
 
     private static final String OUTPUT_DIR = "sp_alarms";
-    private static final String ALARM_DELIMITER = "\t";
 
     public static ArrayList<Alarm> getAlarmsFromStorage(Context ctx) {
-        Log.i(LOG_TAG, "Retrieving alarm records from storage!");
+        Log.i(Constants.LOG_TAG, "Retrieving alarm records from storage!");
         ArrayList<Alarm> alarms = new ArrayList<>();
-        String[] guids = StorageUtil.readFileFromStorage(ctx,
-                SP_KEY_GUIDS).split(ALARM_DELIMITER);
+        File alarmsDir = verifyOutputDir(ctx);
         int alarmCount = 0;
 
-        for (String guid : guids) {
-            if (guid != "" && guid != " ") {
-                String jsonAlarm = StorageUtil.readFileFromStorage(ctx, guid);
-                Alarm alarm = Alarm.importFromJson(jsonAlarm);
-                alarm.setID(alarmCount);
-                alarms.add(alarm);
-                alarmCount++;
-            }
+        for (String alarmFile : alarmsDir.list()) {
+            Log.i(Constants.LOG_TAG, "Scanning file: " + alarmFile);
+            String jsonAlarm = StorageUtil.readFileFromStorage(ctx, alarmFile);
+            Alarm alarm = Alarm.importFromJson(jsonAlarm);
+            alarm.setID(alarmCount);
+            alarms.add(alarm);
+            alarmCount++;
         }
 
         return alarms;
     }
 
     public static void writeAlarmsToStorage(Context ctx, ArrayList<Alarm> alarms) {
-        String guidListString = "";
         for (Alarm alarm : alarms) {
-            guidListString += (alarm.getGuid() + ALARM_DELIMITER);
             String jsonAlarm = Alarm.exportToJson(alarm);
-            Log.i(LOG_TAG, "Writing alarm to storage: " + jsonAlarm);
+            Log.i(Constants.LOG_TAG, "Writing alarm to storage: " + jsonAlarm);
             StorageUtil.writeFileToStorage(ctx, alarm.getGuid(), jsonAlarm);
         }
-
-        StorageUtil.writeFileToStorage(ctx, SP_KEY_GUIDS, guidListString);
     }
 
     public static void deleteFileFromStorage(Context ctx, String filename) {
@@ -61,18 +51,18 @@ public class StorageUtil {
             if (outputFile.exists())
                 outputFile.delete();
             else
-                Log.e(LOG_TAG, "Delete file: " + filename
+                Log.e(Constants.LOG_TAG, "Can't delete file: " + filename
                         + "\t if it doesn't exist!");
 
-            Log.i(LOG_TAG, "Deleted file: " + outputFile.getAbsolutePath());
+            Log.i(Constants.LOG_TAG, "Deleted file: " + outputFile.getAbsolutePath());
         } catch (Exception ex) {
-            Log.e(LOG_TAG, "An error occurred while deleting file: "
+            Log.e(Constants.LOG_TAG, "An error occurred while deleting file: "
                     + filename, ex);
         }
     }
 
     public static ArrayList<SurveyQuestion> getSurveyQuestionsFromStorage() {
-        Log.i(LOG_TAG, "Retrieving survey questions from storage!");
+        Log.i(Constants.LOG_TAG, "Retrieving survey questions from storage!");
 
         // TODO - populate survey question list for real
         ArrayList<SurveyQuestion> questions = new ArrayList<>();
@@ -93,14 +83,14 @@ public class StorageUtil {
                 BufferedReader br = new BufferedReader(new FileReader(outputFile));
                 while ((line = br.readLine()) != null) contents += line;
             } else {
-                Log.e(LOG_TAG, "Cannot read from file: "
+                Log.e(Constants.LOG_TAG, "Cannot read from file: "
                         + filename + "\t if it doesn't exist!");
             }
 
-            Log.i(LOG_TAG, "Read contents: " + contents
+            Log.i(Constants.LOG_TAG, "Read contents: " + contents
                     + "\n \t from file: " + outputFile.getAbsolutePath());
         } catch (Exception ex) {
-            Log.e(LOG_TAG, "An error occurred while reading data from file: "
+            Log.e(Constants.LOG_TAG, "An error occurred while reading data from file: "
                     + filename, ex);
         }
 
@@ -120,10 +110,10 @@ public class StorageUtil {
             writer.flush();
             writer.close();
 
-            Log.i(LOG_TAG, "Wrote contents: " + content
+            Log.i(Constants.LOG_TAG, "Wrote contents: " + content
                     + "\n \t to file: " + outputFile.getAbsolutePath());
         } catch (Exception ex) {
-            Log.e(LOG_TAG, "An error occurred while writing data to file: "
+            Log.e(Constants.LOG_TAG, "An error occurred while writing data to file: "
                     + filename, ex);
         }
     }
@@ -131,7 +121,7 @@ public class StorageUtil {
     private static File verifyOutputDir(Context ctx) {
         File outputDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), OUTPUT_DIR);
         if (!outputDir.exists()) {
-            Log.e(LOG_TAG, "Output dir does not exist.  Creating output dir: "
+            Log.e(Constants.LOG_TAG, "Output dir does not exist.  Creating output dir: "
                     + outputDir.getAbsolutePath());
             outputDir.mkdir();
         }
